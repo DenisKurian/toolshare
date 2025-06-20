06cf2e8a010d2d11122c4b58827f4a64422edf4d
Denis Kurian Deepu <deniskuriandeepu@gmail.com>
2025-06-20 23:25:34 +0530

    Deployment

--- Tool/settings.py
+++ Tool/settings.py
@@ -131,9 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static',  # for development
 ]
-STATIC_ROOT = BASE_DIR / ''
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic in production
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
