88642f71abfbfac7228b862e6562136b2a25586a
Denis Kurian Deepu <deniskuriandeepu@gmail.com>
2025-06-20 23:17:40 +0530

    Deployment

--- Tool/settings.py
+++ Tool/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / ''
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
